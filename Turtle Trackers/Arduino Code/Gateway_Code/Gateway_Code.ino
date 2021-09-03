01fdef91f3a678c7323fa38e93621f5f589ce502
Waseef Nayeem <nayeemw@mcmaster.ca>
2021-09-03 15:24:09 -0400

    Updated comments and added README

--- Turtle Trackers/Arduino Code/Gateway_Code/Gateway_Code.ino
+++ Turtle Trackers/Arduino Code/Gateway_Code/Gateway_Code.ino
@@ -17,12 +17,14 @@ String endpoint = "/data";
 void setup() {
   pinMode(SIM3G_EN, OUTPUT);
 
+  // Power cycle 3G module to reset it
   digitalWrite(SIM3G_EN, HIGH);
   delay(POWER_ON_TIME);
   digitalWrite(SIM3G_EN, LOW);
 
   delay(1000);
 
+  // Turn it off to save power
   digitalWrite(SIM3G_EN, HIGH);
   delay(4000);
   digitalWrite(SIM3G_EN, LOW);
@@ -38,6 +40,7 @@ void setup() {
     while (1);
   }
 
+  // Enabled LoRa module's built in error checking
   LoRa.enableCrc();
   
   ss.begin(9600);
@@ -45,13 +48,16 @@ void setup() {
 }
 
 void loop() {
+  // Listen for packets
   int packetSize = LoRa.parsePacket();
   String result;
   if (packetSize) {
+	// Parse header values
     byte sender = LoRa.read();
     byte msgNum = LoRa.read();
     byte msgLen = LoRa.read();
 
+	// Read packet message into String
     while (LoRa.available()) {
       result += (char)LoRa.read();
     }
@@ -61,26 +67,36 @@ void loop() {
     start3G();
     postToServer(altHost, 8080, sender, msgNum, result);
     stop3G();
+	
+	// TODO: Save packet if failed to send
   } else {
-    Serial.flush();
-    LoRa.sleep();
-    for (int i = 0; i < 2; i++) {
-      LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
-    }
+	// Enter Low Power Mode
+	// This currently causes the gateway to function incorrectly, exact cause is not determinable
+    // Serial.flush();
+    // LoRa.sleep();
+    // for (int i = 0; i < 2; i++) {
+    //   LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    // }
   }
 }
 
+// Creates a POST request and sends it over 3G to the webserver
 void postToServer(String host, int port, byte id, byte count, String message) {
+  // Session Open Request
   char sessCommand[255];
-  sprintf(sessCommand, "AT+CHTTPSOPSE=\"%s\",%d,1", host.c_str(), port);
+  sprintf(sessCommand, "AT+CHTTPSOPSE=\"%s\",%d,1", host.c_str(), port);	
   
+  // Try to open an HTTP session
   while (!sendCommand(sessCommand, 10000).equals("OK"));
   
+  // Build POST request
   char command[255];
   sprintf(command, "POST /data HTTP/1.1\r\nHost: %s\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 30\r\n\r\nid=%d&count=%d&msg=%s", host.c_str(), id, count, message.c_str());
   
+  // TODO: Figure out exact length of the request
   sendCommand("AT+CHTTPSSEND=143", 250);
   
+  // Send the request and wait for a response
   sendCommand(command, 8000);
   sendCommand("AT+CHTTPSRECV?", 8000);
   sendCommand("AT+CHTTPSRECV=2000", 8000);
@@ -88,12 +104,14 @@ void postToServer(String host, int port, byte id, byte count, String message) {
 }
 
 void start3G() {
+  // Turn on the 3G module
   digitalWrite(SIM3G_EN, HIGH);
   delay(POWER_ON_TIME);
   digitalWrite(SIM3G_EN, LOW);
   
   delay(15000);
-
+  
+  // Initialize the 3G module
   sendCommand("AT", 250);
   sendCommand("ATE0", 250);
   sendCommand("AT+CMEE=2", 250);
@@ -101,6 +119,7 @@ void start3G() {
   sendCommand("AT+CGSOCKCONT=1,\"IP\",\"rogers-core-appl1.apn\"", 250);
   sendCommand("AT+CSOCKSETPN=1", 250);
   
+  // Attempt to start the HTTPS service
   while (sendCommand("AT+CHTTPSSTART", 10000).equals("ERROR")) {
     sendCommand("AT+CHTTPSCLSE", 10000);
     sendCommand("AT+CHTTPSSTOP", 10000);
@@ -110,13 +129,16 @@ void start3G() {
 }
 
 void stop3G() {
+  // Stop the HTTPS service
   sendCommand("AT+CHTTPSSTOP", 2000);
   
+  // Turn off the module
   digitalWrite(SIM3G_EN, HIGH);
   delay(4000);
   digitalWrite(SIM3G_EN, LOW);
 }
 
+// Helper function used to simplify sending AT commands and receiving responses
 String sendCommand(const char *command, unsigned long timeout) {
   String result;
   Serial.print(">> ");
---
+++ Turtle Trackers/Arduino Code/README.md
@@ -0,0 +1,7 @@
+# Overview
+Code used in the Turtle Tracker project, 2021 version. This version differs from the previous version located in `Node_Code.ino`.
+
+`Gateway_Code` is for use with the Gateway module which consists of an Arduino UNO, LoRA shield and 3G Shield.
+`Tracker_Code` is for use with the Turtle Tracker module which consists of a Moteino and GPS module.
+
+Full hardware specification is documented in https://gitlab.cas.mcmaster.ca/re-mote/arduino-motes/-/tree/master/Documentation/Setup
